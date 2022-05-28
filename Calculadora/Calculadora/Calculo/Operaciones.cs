484674aae1f7a7bc15abd6ca5003a9ec8a9e8e4d
liliana2327 <23hernandezlili23@gmail.com>
2022-05-27 18:16:23 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculadora/Calculo/Operaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.Calculo
+{
+    class Operaciones
+    {
+        public Decimal numero1 { get; set; }
+        public Decimal numero2 { get; set; }
+
+
+        public Decimal suma(Decimal a, Decimal b)
+        {
+            return a + b;
+        }
+
+        public Decimal resta(Decimal a, Decimal b)
+        {
+            return a - b;
+        }
+
+        public Decimal multiplicacion(Decimal a, Decimal b)
+        {
+            return a * b;
+        }
+
+        public Decimal division(Decimal a, Decimal b)
+        {
+            Decimal result = a / b;
+            return result;
+        }
+    }
+}
---
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        Calculo.Operaciones cal = new Calculo.Operaciones();
+        public Decimal n1;
+        public Decimal n2;
+        public Decimal result;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsuma_Clicked(object sender, EventArgs e)
+        {
+             n1 = Convert.ToDecimal(num1.Text);
+             n2 = Convert.ToDecimal(num2.Text);
+
+            result = cal.suma (n1, n2);
+
+            await Navigation.PushAsync(new Resultado(result));
+        }
+
+        private async void btnresta_Clicked(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDecimal(num1.Text);
+            n2 = Convert.ToDecimal(num2.Text);
+
+            result = cal.resta(n1, n2);
+
+            await Navigation.PushAsync(new Resultado(result));
+        }
+
+        private async void btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDecimal(num1.Text);
+            n2 = Convert.ToDecimal(num2.Text);
+
+            result = cal.multiplicacion(n1, n2);
+
+            await Navigation.PushAsync(new Resultado(result));
+        }
+
+        private async void btndivision_Clicked(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDecimal(num1.Text);
+            n2 = Convert.ToDecimal(num2.Text);
+
+            result = cal.division(n1, n2);
+
+            await Navigation.PushAsync(new Resultado(result));
+        }
+    }
+}
